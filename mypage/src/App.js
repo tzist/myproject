c01784652c9cabc8299f260de36ed70b2578db0a
hooram.seo <gnfka0828@gmail.com>
2019-05-15 15:55:24 +0900

    mypage first commit

---
+++ mypage/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Example from './js/example';
+
+function App() {
+  return (
+    <div>
+      <Example></Example>
+    </div>
+  );
+}
+
+export default App;
