ceaf76d75c527a65a268326ba7dc6b171e088cf1
hooram.seo <gnfka0828@gmail.com>
2019-05-30 10:57:59 +0900

    2019-05-30 : ReactStrap - Breadcrumb 추가
    :Detailed Notes:
    1) ReactStrap - Breadcrumb 추가
    
    :Reference:
    브레드크럼(빵부스러기로 표시한 길)이란 핸젤과 그레텔에서 따온 용어로, 사이트나 웹 앱에서 유저의 위치를 보여주는 부차적인 내비게이션 시스템을 뜻한다고 합니다.

--- mypage/src/App.js
+++ mypage/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Carousel1 from './js/Carousel1';
 import Alert3 from './js/Alert/Alert3';
 import Badge3 from './js/Badge/Badge3';
+import Breadcrumb2 from './js/Breadcrumb/Breadcrumb2';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Carousel1></Carousel1>
       <Alert3></Alert3>
       <Badge3></Badge3>
+      <Breadcrumb2></Breadcrumb2>
     </div>
   );
 }
