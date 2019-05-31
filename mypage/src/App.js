0e6c79ad9c8de037deeaa21bb6bf41cf23b3e850
hooram.seo <gnfka0828@gmail.com>
2019-05-31 16:08:52 +0900

    2019-05-31 : ReactStrap - Collapse 추가
    :Detailed Notes:
    1) ReactStrap - Collapse 추가
    
    :Reference:

--- mypage/src/App.js
+++ mypage/src/App.js
@@ -8,6 +8,7 @@ import ButtonDropdown6 from './js/ButtonDropdown/ButtonDropdown6';
 import ButtonGroup2 from './js/ButtonGroup/ButtonGroup2';
 import Button5 from './js/Button/Button5';
 import Card8 from './js/Card/Card8';
+import Collapse2 from './js/Collapse/Collapse2';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
       <ButtonGroup2></ButtonGroup2>
       <Button5></Button5>
       <Card8></Card8>
+      <Collapse2></Collapse2>
     </div>
   );
 }
