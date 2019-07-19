afb58761723667a61c1848cd782a5df8789ad23f
hooram.seo <gnfka0828@gmail.com>
2019-07-19 14:35:34 +0900

    2019-07-19 : react practice 프로젝트- 3.3
    :Detailed Notes:
    1) react practice 프로젝트- 3.3
    
    :Reference:

--- react_practice/hello-react/src/MyComponent.js
+++ react_practice/hello-react/src/MyComponent.js
@@ -10,12 +10,37 @@ class MyComponent extends Component {
         name: PropTypes.string,
         age: PropTypes.number.isRequired
     }
+
+    /*
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            number: 0
+        }
+    }
+    */
+
+    state = {
+        number: 0
+    }
     
     render() {
         return (
             <div>
                 나의 새롭고 멋진 컴포넌트,
                 안녕하세요! 제 이름은 {this.props.name} ({this.props.age}) 입니다!
+                숫자는 {this.state.number} 이다!
+
+            <br/>
+            <button onClick={() => {
+                this.setState({
+                    number: this.state.number + 1
+                })
+            }
+            }>
+                더하기
+            </button>
             </div>
         )
     }
