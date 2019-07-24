bbbe8cfd8415689a1f975b126ec2e4ef37ce63c9
hooram.seo <gnfka0828@gmail.com>
2019-07-24 16:14:25 +0900

    2019-07-24 : react practice 프로젝트 - 7
    :Detailed Notes:
    1) react practice 프로젝트 - 7
    
    :Reference:

--- react_practice/hello-react/src/App.js
+++ react_practice/hello-react/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ValidationSample from './ValidationSample';
@@ -6,10 +6,38 @@ import MyComponent from './MyComponent';
 import EventPractice from './EventPractice';
 import ScrollBox from './ScrollBox';
 import IterationSample from './IterationSample';
+import LifeCycleSample from './LifeCycleSample';
 
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+class App extends Component {
+  state = {
+    color: '#000000'
+  }
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <LifeCycleSample color={this.state.color}/>
+      </div>
+    );
+  }
+}
+
+/*
 function App() {
   return (
     <div className="App">
+      <LifeCycleSample />
       <IterationSample />
       <ScrollBox />
       <ValidationSample />
@@ -34,5 +62,6 @@ function App() {
     </div>
   );
 }
+*/
 
 export default App;
