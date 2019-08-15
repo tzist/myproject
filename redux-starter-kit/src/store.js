422df0dc71d0be2533cd447878302ee1229130f9
hooram.seo <gnfka0828@gmail.com>
2019-08-15 19:00:13 +0900

    2019-08-15 : react practice 프로젝트 - 15.1(2)
    :Detailed Notes:
    1) react practice 프로젝트 - 15.1(2)
    
    :Reference:
    1) redux-logger(미들웨어 라이브러리) : https://github.com/LogRocket/redux-logger

--- redux-starter-kit/src/store.js
+++ redux-starter-kit/src/store.js
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
 import modules from './modules';
-import loggerMiddleware from './lib/loggerMiddleware';
 
-// 미들웨어가 여러 개일 때는 파라미터로 전달하면 됩니다.
-// ex) applyMiddleware(a, b, c)
-// 미들웨어 순서는 여기에서 전달한 파라미터 순서대로 지정합니다.
-const store = createStore(modules, applyMiddleware(loggerMiddleware))
+import { createLogger } from 'redux-logger';
+
+/* 로그 미들웨어를 만들 때 설정을 커스터마이징 할 수 있습니다. */
+const logger = createLogger();
+
+const store = createStore(modules, applyMiddleware(logger))
 
 export default store;
