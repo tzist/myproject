b869bce1355325239312c6db780c026302c80551
hooram.seo <gnfka0828@gmail.com>
2019-11-22 16:34:46 +0900

    2019-11-22 : react practice 프로젝트 - 18(18.1 - 18.4)
    :Detailed Notes:
    2019-11-22 : react practice 프로젝트 - 18(18.1 - 18.4)
    
    :Reference:

---
+++ blog/blog-backend/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    es6: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    "no-unused-vars": 1,
+    "comma-dangle": 0,
+    "eol-last": 0,
+    "no-console": 0,
+    "linebreak-style": 0,
+  },
+};
