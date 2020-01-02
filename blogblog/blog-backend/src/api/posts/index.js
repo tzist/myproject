9083c03bc943d32015bc649d26aaa6a90205cd3e
hooram.seo <gnfka0828@gmail.com>
2020-01-02 14:54:01 +0900

    2020-01-02 : react practice 프로젝트 - blog(DB 연동)_2
    :Detailed Notes:
    2020-01-02 : react practice 프로젝트 - blog(DB 연동)_2
    
    :Reference:

--- blogblog/blog-backend/src/api/posts/index.js
+++ blogblog/blog-backend/src/api/posts/index.js
@@ -1,6 +1,5 @@
 const Router = require('koa-router');
 const postsCtrl = require('./posts.ctrl');
-
 const posts = new Router();
 
 /*
@@ -15,9 +14,9 @@ const printInfo = (ctx) => {
 
 posts.get('/', postsCtrl.list);
 posts.post('/', postsCtrl.write);
-posts.get('/:id', postsCtrl.read);
-posts.delete('/:id', postsCtrl.remove);
+posts.get('/:id', postsCtrl.checkObjectId, postsCtrl.read);
+posts.delete('/:id', postsCtrl.checkObjectId, postsCtrl.remove);
 //posts.put('/:id', postsCtrl.replace);
-posts.patch('/:id', postsCtrl.update);
+posts.patch('/:id', postsCtrl.checkObjectId, postsCtrl.update);
 
 module.exports = posts;
